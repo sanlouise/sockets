8a62ac56d8616996323eef338d483d0242d44c6d
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-09 17:22:48 +0200

    Add jQuery and Socket.io libraries

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 //Front-end
 app.use(express.static(__dirname + '/public'));
+
+//Listen to events, here connection
+io.on('connection', function () {
+	console.log('User is connected via socket.io!');
+});
 
 http.listen(PORT, function () {
 	console.log('Server started!');
