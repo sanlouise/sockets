bd8cf608c090dce77be1d727d390d472652bfd94
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-11 19:56:11 +0200

    Add room names to chat

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,8 @@ var room = getQueryVariable('room');
 
 var socket = io();
 
+jQuery('.room-name').text(room);
+
 //Listen for connect event
 socket.on('connect', function () {
 	console.log('Connected to the socket.io server!');
@@ -12,11 +14,8 @@ socket.on('connect', function () {
 socket.on('message', function (message) {
 	var momentTimestamp = moment.utc(message.timestamp);
 	var $message = jQuery('.messages')
-	console.log('New message:');
-	console.log(message.text);
 
 	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
-	//Adding incoming messages to the screen
 	$message.append('<p>' + message.text + '</p>');
 
 });
