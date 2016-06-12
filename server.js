97410b28355828a6db1e196925fde404ff868a8e
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-12 10:30:42 +0200

    Create current users command

--- server.js
+++ server.js
@@ -11,13 +11,41 @@ app.use(express.static(__dirname + '/public'));
 //To store room name user is in
 var clientInfo = {};
 
+//Sends current users to socket
+function sendCurrentUsers (socket) {
+	//Check what room the user is in
+	var info = clientInfo[socket.id];
+	//Empty array to push users in room to
+	var users = [];
+
+	//Check if chat room still exists
+	if (typeof info === 'undefined') {
+		return;
+	}
+	//Search through clientInfo. Takes object and returns array of all attributes on that object. 
+	// Here we check if room name is equal to current user's room.
+	Object.keys(clientInfo).forEach(function (socketId) {
+		var userInfo = clientInfo[socketId];
+
+		if (info.room === userInfo.room) {
+			users.push(userInfo.name);
+		}
+	});
+	//Emit all the names of the people in the same room
+	socket.emit('message', {
+		name: 'System',
+		text: 'Users in chat room: ' + users.join(', '),
+		timestamp: moment().valueOf()
+	})
+}
+
 //Listen to events, here a connection
 //Socket is an individual client (a computer) that emits something to a server
-io.on('connect', function (socket) {
+io.on('connect', function(socket) {
 	console.log('User is connected via socket.io!');
 
 	// To disconnect. This is an inbuilt method.
-	socket.on('disconnect', function () {
+	socket.on('disconnect', function() {
 		var userData = clientInfo[socket.id];
 		//Check if connected in a chat room
 		if (typeof userData !== 'undefined') {
@@ -32,7 +60,7 @@ io.on('connect', function (socket) {
 		}
 	});
 
-	socket.on('joinRoom', function (req) {
+	socket.on('joinRoom', function(req) {
 		//Stores dynamic room name with a unique identifier
 		clientInfo[socket.id] = req;
 		//Join is specific to sockets, tells socket.io library to connect socket to specific room
@@ -46,13 +74,23 @@ io.on('connect', function (socket) {
 
 	});
 
-	socket.on('message', function (message) {
+	socket.on('message', function(message) {
+
+		//Check if a message is a commend, otherwise run default message
+		if (message.text === '@currentUsers') {
+			sendCurrentUsers(sockets);
+
+		} else {
+			//Add timestamps. valueOf returns Javascript timestamp, ms version of UNIX timestamp.
+			message.timestamp = moment().valueOf();
+			//Sends to everyone except for the client that sent it. Only to the room of the current user!
+			io.to(clientInfo[socket.id].room).emit('message', message);
+
+		}
+
 		//Emit event to other connections
 		console.log('Your message was received: ' + message.text);
-		//Add timestamps. valueOf returns Javascript timestamp, ms version of UNIX timestamp.
-		message.timestamp = moment().valueOf();
-		//Sends to everyone except for the client that sent it. Only to the room of the current user!
-		io.to(clientInfo[socket.id].room).emit('message', message);
+
 	});
 
 	socket.emit('message', {
@@ -62,6 +100,6 @@ io.on('connect', function (socket) {
 	});
 });
 
-http.listen(PORT, function () {
+http.listen(PORT, function() {
 	console.log('Server started!');
 });
