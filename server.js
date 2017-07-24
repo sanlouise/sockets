d8daa49afc467c4088740a1f009e4dd1de3d925e
sanlouise <sandra.adamshallie@gmail.com>
2017-07-24 10:22:11 -0400

    Refactor server.js

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
-const PORT = process.env.PORT || 3000;
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const moment = require('moment');
+const PORT = process.env.PORT || 3000,
+express = require('express'),
+app = express(),
+http = require('http').Server(app),
+io = require('socket.io')(http),
+moment = require('moment');
 
 //Front-end
 app.use(express.static(__dirname + '/public'));
 
 //To store room name user is in
-let clientInfo = {};
+let roomId = {};
 
 //Sends current users to socket
-const sendCurrentUsers = (socket) => {
+const connectUsers = (socket) => {
 	//Check what room the user is in
-	const info = clientInfo[socket.id];
+	const socketId = roomId[socket.id];
 	//Empty array to push users in room to
 	let users = [];
 
 	//Check if chat room still exists
-	if (typeof info === 'undefined') {
+	if (typeof socketId === 'undefined') {
 		return;
 	}
-	//Search through clientInfo. Takes object and returns array of all attributes on that object.
+	//Search through roomId. Takes object and returns array of all attributes on that object.
 	// Here we check if room name is equal to current user's room.
-	Object.keys(clientInfo).forEach(function (socketId) {
-		const userInfo = clientInfo[socketId];
+	Object.keys(roomId).forEach(function (socketId) {
+		const userId = roomId[socketId];
 
-		if (info.room === userInfo.room) {
-			users.push(userInfo.name);
+		if (socketId.room === userId.room) {
+			users.push(userId.name);
 		}
 	});
 	//Emit all the names of the people in the same room
@@ -46,7 +46,7 @@ io.on('connect', (socket) => {
 
 	// To disconnect. This is an inbuilt method.
 	socket.on('disconnect', () => {
-		const userData = clientInfo[socket.id];
+		const userData = roomId[socket.id];
 		//Check if connected in a chat room
 		if (typeof userData !== 'undefined') {
 			socket.leave(userData.room);
@@ -56,13 +56,13 @@ io.on('connect', (socket) => {
 				text: userData.name + ' has left the room.',
 				timestamp: moment().valueOf()
 			});
-			delete clientInfo[socket.id];
+			delete roomId[socket.id];
 		}
 	});
 
 	socket.on('joinRoom', (req) => {
 		//Stores dynamic room name with a unique identifier
-		clientInfo[socket.id] = req;
+		roomId[socket.id] = req;
 		//Join is specific to sockets, tells socket.io library to connect socket to specific room
 		socket.join(req.room);
 		//Tells all members that someone joined the room
@@ -77,12 +77,12 @@ io.on('connect', (socket) => {
 	socket.on('message', (message) => {
 		//Check if a message is a command, otherwise run default message
 		if (message.text === '@currentUsers') {
-			sendCurrentUsers(socket);
+			connectUsers(socket);
 		} else {
 			//Add timestamps. valueOf returns Javascript timestamp, ms version of UNIX timestamp.
 			message.timestamp = moment().valueOf();
 			//Sends to everyone except for the client that sent it. Only to the room of the current user!
-			io.to(clientInfo[socket.id].room).emit('message', message);
+			io.to(roomId[socket.id].room).emit('message', message);
 
 		}
 		//Emit event to other connections
